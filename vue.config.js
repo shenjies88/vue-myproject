1c16f0f8674f44991098876474ca28a2ebf09ef6
shenjies88 <1961486254@qq.com>
2019-07-31 21:40:51 +0800

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,19 +4,7 @@ const resolve = dir => {
     return path.join(__dirname, dir)
 };
 
-// 项目部署基础
-// 默认情况下，我们假设你的应用将被部署在域的根目录下,
-// 例如：https://www.my-app.com/
-// 默认：'/'
-// 如果您的应用程序部署在子路径中，则需要在这指定子路径
-// 例如：https://www.foobar.com/my-app/
-// 需要将它改为'/my-app/'
-const BASE_URL = process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/';
-
 module.exports = {
-    baseUrl: BASE_URL,
     // 如果你不需要使用eslint，把lintOnSave设为false即可
     lintOnSave: false,
     chainWebpack: config => {
